0e57f6063802df7559f700f0e76deb6322f7bd89
Leonardo Oliveros <leonardoliveros.lo@gmail.com>
2021-08-01 16:57:40 -0500

    initial commit

---
+++ imports/ui/components/TaskForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form class="task-form" @submit.prevent="handleSubmit">
+    <input
+        type="text"
+        placeholder="Type to add new tasks"
+        v-model="newTask"
+    />
+    <button type="submit">Add Task</button>
+  </form>
+</template>
+
+<script>
+import { Meteor } from 'meteor/meteor';
+
+export default {
+  data() {
+    return {
+      newTask: ""
+    }
+  },
+  methods: {
+    handleSubmit(event) {
+      if (this.newTask.length === 0) return;
+      Meteor.call('tasks.insert', this.newTask.trim());
+      // Clear form
+      this.newTask = "";
+    }
+  },
+}
+</script>
